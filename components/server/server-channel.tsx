42b5e58c9b0e3dfa11983fb008948bddf10c64a8
Aditya Kumar Saini <adityasainics10@gmail.com>
2024-07-11 11:53:42 +0530

    done with basic serverId and channelId page setup

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -5,7 +5,7 @@ import { Edit, Hash, Lock, Mic, Trash, Video } from 'lucide-react';
 import { useParams, useRouter } from 'next/navigation';
 import React from 'react'
 import { ActionTooltip } from '../action-tooltip';
-import { useModal } from '@/hooks/use-model-state';
+import { ModalType, useModal } from '@/hooks/use-model-state';
 
 interface ServerChannelProps{
     channel:Channel;
@@ -27,11 +27,22 @@ function ServerChannel({channel,server,role}:ServerChannelProps) {
 
     const Icon = iconMap[channel.type]
 
+    const onClick=()=>{
+       router.push(`/servers/${params?.serverId}/channels/${channel.id}`)
+    }
+
+    const onAction=(e:React.MouseEvent,action:ModalType)=>{
+      e.stopPropagation();
+      onOpen(action,{channel,server});
+    }
+
     return (
-      <div 
-       onClick={()=>{}}
+      <button 
+       onClick={onClick}
        className={cn(
         'group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1',
+         params?.channelId=== channel.id && 'bg-zinc-700/20 dark:bg-zinc-700'
+
 
        )}
       >
@@ -48,14 +59,14 @@ function ServerChannel({channel,server,role}:ServerChannelProps) {
         {channel.name !== "general" && role !== MemeberRole.GUEST &&(
           <div className='ml-auto flex items-center gap-x-2'>
             <ActionTooltip label="Edit">
-              <Edit onClick={()=>onOpen("editChannel",{channel,server})} className='
+              <Edit onClick={(e)=>onAction(e,"editChannel")} className='
               invisible group-hover:visible w-4 h-4 text-zinc-500 
               hover:text-zinc-600 dark:text-zinc-400 
               dark:hover:text-zinc-300 transition'/>
             </ActionTooltip>
 
             <ActionTooltip label="Delete">
-              <Trash onClick={()=>onOpen("deleteChannel",{channel,server})} className='
+              <Trash onClick={(e)=>onAction(e,"deleteChannel")} className='
               invisible group-hover:visible w-4 h-4 text-zinc-500 
               hover:text-zinc-600 dark:text-zinc-400 
               dark:hover:text-zinc-300 transition'/>
@@ -70,7 +81,7 @@ function ServerChannel({channel,server,role}:ServerChannelProps) {
           />
         )}
         
-      </div>
+      </button>
     )
 }
 
